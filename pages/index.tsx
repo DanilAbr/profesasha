984aeee504b597189aaca27e6003f643131581e6
Semyon <saymon221090@gmail.com>
2021-06-23 16:50:51 +0300

    настраивает поведении стрелки pageDown на главной странице

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,15 +2,29 @@ import DefaultPage from "../layouts/default-page/default-page";
 import AboutMe from "../components/about-me/about-me";
 import FirstScreen from "../components/first-screen/first-screen";
 import Link from "next/link";
+import PageDownButton from '../components/atoms/page-down-button/page-down-button';
+import {useRef} from 'react';
 
 export default function Home() {
+  const aboutMeSection = useRef<HTMLDivElement>(null);
+
+  function scrollDown() {
+    if ( aboutMeSection.current ) {
+      aboutMeSection.current.scrollIntoView({ behavior: 'smooth' });
+    }
+  }
+
   return (
     <DefaultPage pageModifier='index'>
+      <PageDownButton onClick={ scrollDown } />
       <div className='index'>
         <div className='index__first-screen'>
           <FirstScreen />
         </div>
-        <div className='index__about-me'>
+        <div
+          className='index__about-me'
+          ref={ aboutMeSection }
+        >
           <AboutMe />
         </div>
         <Link href='/class-options'>
@@ -22,5 +36,3 @@ export default function Home() {
     </DefaultPage>
   )
 }
-
-// TODO: menu esc
