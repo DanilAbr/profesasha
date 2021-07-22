ed6998788102160fafd3374e42ea6108e65b5f08
Danila <Ltdfm@mail.ru>
2021-07-22 12:55:52 +0300

    test

--- components/first-screen/first-screen.tsx
+++ components/first-screen/first-screen.tsx
@@ -2,27 +2,27 @@ export default function FirstScreen() {
   return (
     <section className="first-screen">
       <div className="first-screen__container">
-        <div className="first-screen__img-wrapper">
-          <picture>
-            <source
-              media="(min-width: 1152px)"
-              srcSet="assets/images/index-first-photo-desktop@1x.png 1x, assets/images/index-first-photo-desktop@2x.png 2x"
-            />
-            <source
-              media="(min-width: 768px)"
-              srcSet="assets/images/index-first-photo-tablet@1x.png 1x, assets/images/index-first-photo-tablet@2x.png 2x"
-            />
-            <img
-              className="first-screen__img"
-              loading='lazy'
-              src="assets/images/index-first-photo-mobile@1x.png"
-              width='378'
-              height='403'
-              srcSet="assets/images/index-first-photo-mobile@2x.png 2x"
-              alt="Александра Абрамова"
-            />
-          </picture>
-        </div>
+        {/*<div className="first-screen__img-wrapper">*/}
+        {/*  <picture>*/}
+        {/*    <source*/}
+        {/*      media="(min-width: 1152px)"*/}
+        {/*      srcSet="assets/images/index-first-photo-desktop@1x.png 1x, assets/images/index-first-photo-desktop@2x.png 2x"*/}
+        {/*    />*/}
+        {/*    <source*/}
+        {/*      media="(min-width: 768px)"*/}
+        {/*      srcSet="assets/images/index-first-photo-tablet@1x.png 1x, assets/images/index-first-photo-tablet@2x.png 2x"*/}
+        {/*    />*/}
+        {/*    <img*/}
+        {/*      className="first-screen__img"*/}
+        {/*      loading='lazy'*/}
+        {/*      src="assets/images/index-first-photo-mobile@1x.png"*/}
+        {/*      width='378'*/}
+        {/*      height='403'*/}
+        {/*      srcSet="assets/images/index-first-photo-mobile@2x.png 2x"*/}
+        {/*      alt="Александра Абрамова"*/}
+        {/*    />*/}
+        {/*  </picture>*/}
+        {/*</div>*/}
 
         <div className="first-screen__content-wrapper">
           <p className="first-screen__text first-screen__text--top">Привет, меня зовут</p>
