580f14f44f701f90d86093aa427be7246ba8616c
Danila <Ltdfm@mail.ru>
2021-07-25 17:54:58 +0300

    Рефакторит код, правки в блоге

--- pages/article/[id].tsx
+++ pages/article/[id].tsx
@@ -25,32 +25,29 @@ export default function Article() {
   return (
     <DefaultPage pageModifier="article">
       <div className="article">
-        <div className="article__button-back article__button-back--top">
-          <ButtonBack />
-        </div>
         <article className="article__content">
           <div className="article__item">
             <p className="article__category">{ category.name }</p>
             <h1 className="article__title">{ title }</h1>
             { img &&
-            <div className="article__image-wrapper">
-              <picture>
-                <source
-                  media="(min-width: 1152px)"
-                  srcSet={ ` ${ img.default.desktop } 1x, ${ img.retina.desktop } 2x ` }
-                />
-                <source
-                  media="(min-width: 768px)"
-                  srcSet={ ` ${ img.default.tablet } 1x, ${ img.retina.tablet } 2x ` }
-                />
-                <img
-                  className="article__image"
-                  src={ img.default.mobile }
-                  srcSet={ ` ${ img.retina.mobile } 2x ` }
-                  alt={ title }
-                />
-              </picture>
-            </div>
+              <div className="article__image-wrapper">
+                <picture>
+                  <source
+                    media="(min-width: 1152px)"
+                    srcSet={ ` ${ img.default.desktop } 1x, ${ img.retina.desktop } 2x ` }
+                  />
+                  <source
+                    media="(min-width: 768px)"
+                    srcSet={ ` ${ img.default.tablet } 1x, ${ img.retina.tablet } 2x ` }
+                  />
+                  <img
+                    className="article__image"
+                    src={ img.default.mobile }
+                    srcSet={ ` ${ img.retina.mobile } 2x ` }
+                    alt={ title }
+                  />
+                </picture>
+              </div>
             }
             <p className="article__subtitle">
               { subtitle }
