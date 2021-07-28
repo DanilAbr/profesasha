3c2f20f1c1da9a3f288f52f303fa36a2846f6e26
Danila <Ltdfm@mail.ru>
2021-07-28 09:53:05 +0300

    Удаляет loading="lazy" с первой фото

--- components/first-screen/first-screen.tsx
+++ components/first-screen/first-screen.tsx
@@ -14,7 +14,6 @@ export default function FirstScreen() {
             />
             <img
               className="first-screen__img"
-              loading="lazy"
               src="assets/images/index-first-photo-mobile@1x.png"
               width="378"
               height="403"
@@ -28,8 +27,8 @@ export default function FirstScreen() {
           <p className="first-screen__text first-screen__text--top">Привет, меня зовут</p>
           <h1 className="first-screen__title">Александра Абрамова</h1>
           <p className="first-screen__text first-screen__text--bottom">
-            Я&nbsp;&mdash; дипломированный преподаватель испанского языка и&nbsp;эксперт <span
-            className="nowrap">по&nbsp;подготовке к&nbsp;DELE.</span><br />
+            Я&nbsp;&mdash; дипломированный преподаватель испанского языка и&nbsp;эксперт
+            <span className="nowrap">по&nbsp;подготовке к&nbsp;DELE.</span><br />
             <br />
             Если хочешь заговорить на&nbsp;самом красивом языке в&nbsp;мире, то&nbsp;ты&nbsp;обратился по&nbsp;адресу!
             У&nbsp;меня за&nbsp;плечами имеется огромный опыт преподавания. Какой бы ни&nbsp;была твоя цель изучения
