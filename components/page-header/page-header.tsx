3b6b703d5053194a3416b596509c54dea75505b8
Danila <Ltdfm@mail.ru>
2021-08-10 17:49:03 +0300

    test

--- components/page-header/page-header.tsx
+++ components/page-header/page-header.tsx
@@ -2,26 +2,40 @@ import MainNav from '../main-nav/main-nav';
 import { useEffect, useState } from 'react';
 import useEscKeyListener from '../../hooks/useEscKeyListener';
 import useNonTargetMouseClickListener from '../../hooks/useNonTargetMouseClickListener';
+// @ts-ignore
+import { clearAllBodyScrollLocks, disableBodyScroll, enableBodyScroll } from 'body-scroll-lock';
 
 export default function PageHeader() {
   const [ isMenuOpened, setMenuStatus ] = useState(false);
+  let targetElement:any = null;
 
   useEscKeyListener(() => setMenuStatus(false));
   useNonTargetMouseClickListener(() => setMenuStatus(false), 'page-header');
 
   function changeMenuStatus() {
     setMenuStatus((prev) => !prev);
+
+    if (targetElement) {
+      if (isMenuOpened) disableBodyScroll(targetElement);
+      console.log(targetElement);
+      if (!isMenuOpened) enableBodyScroll(targetElement);
+    }
+
   }
 
   useEffect(() => {
-    const body = document.querySelector('body');
-    const html = window.document.documentElement;
+    if(typeof window !== 'undefined') {
+      const body = document.querySelector('body');
 
-    if (body) {
-      body.style.overflow = (body.clientWidth < 1000 && isMenuOpened)
-        ? 'hidden'
-        : 'auto';
+      if (body && body.clientWidth < 1000 && isMenuOpened) {
+        targetElement = document.querySelector('.page-header__menu-pre-wrapper');
+        body.style.overflow = 'hidden';
+      } else if (body) {
+        body.style.overflow = 'auto';
+      }
     }
+
+    return clearAllBodyScrollLocks();
   }, [ isMenuOpened ]);
 
   return (
